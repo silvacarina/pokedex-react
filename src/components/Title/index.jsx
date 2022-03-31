e440a8ccab6ba36dd3b3c6c81e62972fae581e8d
Carina <cass.carinasilva@gmail.com>
2022-03-31 13:39:34 -0300

    Alterado CSS

--- src/components/Title/index.jsx
+++ src/components/Title/index.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './style.css';
+import pokedeximg from './pokedex.png';
 
 function Logo() {
   return (
-    <div className="logo">
-      <h1 className="logo-img">Pokedex</h1>
+    <div className="title-container">
+      <img className="title-img" src={pokedeximg} alt="Imagem de uma pokedex" />
+      <h1 className="title">Pokedex</h1>
     </div>
   );
 }
